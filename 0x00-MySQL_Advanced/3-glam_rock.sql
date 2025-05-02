127aeeb140fb36291ad99bc3b4e8de244814c003
Christian Chesire <christianck.chesire@gmail.com>
2025-05-02 00:47:29 +0000

    Updated code 3. Old school band, 3-glam_rock.sql.

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,11 @@
 -- Script that lists all bands with Glam rock as their main style, ranked by
 -- their longetivity
 SELECT 
-    name AS band_name, 
+    band_name, 
     CASE 
         WHEN split IS NULL OR split > 2022 THEN 2022 - formed
-	ELSE split - formed
+	ELSE split - formed 
     END AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
